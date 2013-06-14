bb6cdf5a4544a3be479516cb54f02b4995b9c095
Prakriti <itirkarp@gmail.com>
2013-06-15 00:27:18 +1000

    Storing the cart state in the managed bean instead of SFSB and making it session scoped

--- YummyApplication/YummyApplication-war/src/java/beans/MenuManagedBean.java
+++ YummyApplication/YummyApplication-war/src/java/beans/MenuManagedBean.java
@@ -4,41 +4,67 @@ import entity.CartItem;
 import entity.MenuItem;
 import enums.Category;
 import facades.MenuItemFacadeRemote;
-import javax.inject.Named;
+import facades.RestaurantOrderFacadeRemote;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import javax.ejb.EJB;
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import stateful.ShoppingCartRemote;
 
 /**
  *
  * @author Prakriti
  */
 @Named(value = "menuManagedBean")
-@RequestScoped
+@SessionScoped
 public class MenuManagedBean implements Serializable {
     @EJB
-    private ShoppingCartRemote shoppingCart;
-    @EJB
     private MenuItemFacadeRemote menuItemFacade;    
+    @Resource(mappedName = "jms/yummyqueue")
+    private Queue yummyqueue;
+    @Resource(mappedName = "jms/yummyqueueFactory")
+    private ConnectionFactory yummyqueueFactory;
+    @EJB
+    private RestaurantOrderFacadeRemote restaurantOrderFacade;
+    @Inject
+    LoginManagedBean loginBean;
+
     private HashMap<Category, List<MenuItem>> menu;
     private HashMap<Integer, String> selectedItems;
+    private List<CartItem> cartItems;
 
-    public ShoppingCartRemote getShoppingCart() {
-        return shoppingCart;
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public void setCartItems(List<CartItem> cartItems) {
+        this.cartItems = cartItems;
     }
 
     public MenuManagedBean() {
         menu = new HashMap<Category, List<MenuItem>>();   
         selectedItems = new HashMap<Integer, String>();
+        cartItems = new ArrayList<CartItem>();
     }
 
     public HashMap<Integer, String> getSelectedItems() {
@@ -63,14 +89,6 @@ public class MenuManagedBean implements Serializable {
         
     @PostConstruct
     public void init() {
-        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
-        HttpSession session = request.getSession();
-        ShoppingCartRemote sessionCart = (ShoppingCartRemote) session.getAttribute("shoppingCart");
-        if (sessionCart != null) {
-            shoppingCart = sessionCart;
-        } else {
-            session.setAttribute("shoppingCart", shoppingCart); 
-        }
         loadMenuItems();
         loadSelectedItems();
     }
@@ -85,20 +103,22 @@ public class MenuManagedBean implements Serializable {
     }
     
     private void loadSelectedItems() {
-        for (CartItem cartItem : shoppingCart.getItems()) {
+        for (CartItem cartItem : cartItems) {
             selectedItems.put(cartItem.getId(), cartItem.quantity.toString());
         }
     }
     
     public String addToCart() {
-        // If all quantities are 0 don't allow adding to cart
-        shoppingCart.clear();
+        cartItems.clear();
         int numberSelected = 0;
         for (Map.Entry<Integer, String> entry : selectedItems.entrySet()) {
             Integer itemid = entry.getKey();
             Integer quantity = Integer.parseInt(entry.getValue());
             if (quantity != null && quantity > 0) {
-                shoppingCart.add(itemid, quantity);
+                MenuItem item = menuItemFacade.find(itemid);
+                if (item != null) {
+                    cartItems.add(new CartItem(itemid, quantity, item.getName(), item.getPrice()));
+                }
                 numberSelected++;
             }
         }
@@ -109,4 +129,61 @@ public class MenuManagedBean implements Serializable {
         }
         return "success";
     }
+    
+    public List<CartItem> getItems() {
+        return cartItems;
+    }
+
+    public float getOrderTotal() {
+        float total = 0;
+        for (CartItem item : cartItems) {
+            total += item.getPrice() * item.getQuantity();
+        }
+        return total;
+    }
+    
+    public String checkout() {
+        Integer orderId = restaurantOrderFacade.saveOrder(cartItems, loginBean.getCurrentUser());
+        try {
+            sendJMSMessageToYummyqueue(orderId.toString());
+        } catch (JMSException ex) {
+            System.out.println("Could not send message to order email queue: " + ex.getMessage());
+            Logger.getLogger(MenuManagedBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        HttpServletRequest request = (HttpServletRequest) FacesContext
+                .getCurrentInstance().getExternalContext().getRequest();
+        HttpSession session = request.getSession();
+        session.invalidate();
+        return "checkout";
+    }
+
+    private Message createJMSMessageForjmsYummyqueue(Session session, 
+            Object messageData) throws JMSException {
+        TextMessage tm = session.createTextMessage();
+        tm.setText(messageData.toString());
+        return tm;
+    }
+
+    private void sendJMSMessageToYummyqueue(Object messageData) throws JMSException {
+        Connection connection = null;
+        Session session = null;
+        try {
+            connection = yummyqueueFactory.createConnection();
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            MessageProducer messageProducer = session.createProducer(yummyqueue);
+            messageProducer.send(createJMSMessageForjmsYummyqueue(session, messageData));
+        } finally {
+            if (session != null) {
+                try {
+                    session.close();
+                } catch (JMSException e) {
+                    Logger.getLogger(this.getClass().getName()).log(Level.WARNING, "Cannot close session", e);
+                }
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+
 }
